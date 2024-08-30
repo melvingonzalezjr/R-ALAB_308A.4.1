3138fe073d8a622e894ccd6b925fb1edb8dcc56e
melvingonzalezjr <mgonzalez713@gmail.com>
2024-08-30 12:57:06 -0500

    Finish through Question 9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as Carousel from "./Carousel.js";
-import {API_KEY} from "./apiKey.js";
+import { API_KEY } from "./apiKey.js";
 // import axios from "axios";
 
 // The breed selection input element.
@@ -12,7 +12,6 @@ const progressBar = document.getElementById("progressBar");
 const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 
 // Step 0: Store your API key here for reference and easy access.
-
 
 /**
  * 1. Create an async function "initialLoad" that does the following:
@@ -29,32 +28,32 @@ async function initialLoad() {
   const res = await fetch("https://api.thecatapi.com/v1/breeds");
   const jsonData = await res.json();
   // console.log(jsonData);
-
+  //
   jsonData.forEach((obj) => {
     const option = document.createElement("option");
     option.textContent = obj.name;
     option.setAttribute("value", obj.id);
     breedSelect.appendChild(option);
   });
-
-  handleBreedSelect();
+  //
+  axiosHandleBreedSelect();
 }
 
-/**
- * 2. Create an event handler for breedSelect that does the following:
- * - Retrieve information on the selected breed from the cat API using fetch().
- *  - Make sure your request is receiving multiple array items!
- *  - Check the API documentation if you're only getting a single object.
- * //////////////////////////////////////////
- * - For each object in the response array, create a new element for the carousel.
- *  - Append each of these new elements to the carousel.
- * - Use the other data you have been given to create an informational section within the infoDump element.///
- *  - Be creative with how you create DOM elements and HTML.
- *  - Feel free to edit index.html and styles.css to suit your needs, but be careful!
- *  - Remember that functionality comes first, but user experience and design are important.
- * - Each new selection should clear, re-populate, and restart the Carousel.
- * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
- */
+// /**
+//  * 2. Create an event handler for breedSelect that does the following:
+//  * - Retrieve information on the selected breed from the cat API using fetch().
+//  *  - Make sure your request is receiving multiple array items!
+//  *  - Check the API documentation if you're only getting a single object.
+//  * //////////////////////////////////////////
+//  * - For each object in the response array, create a new element for the carousel.
+//  *  - Append each of these new elements to the carousel.
+//  * - Use the other data you have been given to create an informational section within the infoDump element.///
+//  *  - Be creative with how you create DOM elements and HTML.
+//  *  - Feel free to edit index.html and styles.css to suit your needs, but be careful!
+//  *  - Remember that functionality comes first, but user experience and design are important.
+//  * - Each new selection should clear, re-populate, and restart the Carousel.
+//  * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
+//  */
 
 breedSelect.addEventListener("change", handleBreedSelect);
 
@@ -67,6 +66,10 @@ async function handleBreedSelect() {
   const jsonData = await response.json();
   console.log(jsonData);
 
+  /*clear Carousel first upon change.
+   **Then for each object, create CarouselItem using obj's attributes. Append to Carousel.
+   **Get description and set that as the infoDump.textContent to appear
+   */
   Carousel.clear();
   jsonData.forEach((obj) => {
     const element = Carousel.createCarouselItem(
@@ -100,6 +103,95 @@ async function handleBreedSelect() {
  * - As an added challenge, try to do this on your own without referencing the lesson material.
  */
 
+//PART 4, 5
+// set default headers
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+async function axiosHandleBreedSelect() {
+  console.log(breedSelect.value);
+
+  const res = await axios.get(
+    `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breedSelect.value}`,
+    {
+      onDownloadProgress: updateProgress,
+    }
+  );
+
+  // parsed json data
+  const breedsData = res.data;
+  console.log(breedsData);
+
+  // clear the carousel if it has any images
+  if (document.getElementById("carouselInner").firstChild) {
+    Carousel.clear();
+  }
+
+  // create and append images to carousel
+  breedsData.forEach((item) => {
+    const element = Carousel.createCarouselItem(
+      item.url,
+      item.breeds[0].name,
+      item.id
+    );
+    Carousel.appendCarousel(element);
+  });
+
+  // check if there is a child element on the infoDump div
+  if (infoDump.firstChild) {
+    infoDump.firstChild.remove();
+  }
+
+  //TODO: be more creative
+  // create a new element for the info
+  const p = document.createElement("p");
+  p.textContent = breedsData[0].breeds[0].description;
+  infoDump.appendChild(p);
+
+  // TODO
+  Carousel.start();
+}
+
+// Interceptors
+axios.interceptors.request.use((request) => {
+  request.metadata = request.metadata || {};
+  request.metadata.startTime = new Date().getTime();
+
+  console.log("Sending request....");
+  // reset the progressBar to 0
+  progressBar.style.width = "0px";
+  //sets cursor to 'loading progress'
+  document.body.style.cursor = "progress"; //DEBUG!!!!!!
+
+  return request;
+});
+
+//response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    response.config.metadata.endTime = new Date().getTime();
+    response.config.metadata.durationInMS =
+      response.config.metadata.endTime - response.config.metadata.startTime;
+
+    console.log("Response completed....");
+    document.body.style.cursor = "";
+
+    console.log(
+      `Request took ${response.config.metadata.durationInMS} milliseconds.`
+    );
+    return response;
+  },
+  (error) => {
+    error.config.metadata.endTime = new Date().getTime();
+    error.config.metadata.durationInMS =
+      error.config.metadata.endTime - error.config.metadata.startTime;
+
+    console.log(
+      `Request took ${error.config.metadata.durationInMS} milliseconds.`
+    );
+    throw error;
+  }
+);
+
 /**
  * 6. Next, we'll create a progress bar to indicate the request is in progress.
  * - The progressBar element has already been created for you.
@@ -116,11 +208,20 @@ async function handleBreedSelect() {
  *   with for future projects.
  */
 
+function updateProgress(progressEvent) {
+  console.log(progressEvent);
+
+  if (progressEvent.lengthComputable) {
+    progressBar.style.width = progressEvent.total + "px";
+  }
+}
 /**
  * 7. As a final element of progress indication, add the following to your axios interceptors:
  * - In your request interceptor, set the body element's cursor style to "progress."
  * - In your response interceptor, remove the progress cursor style from the body element.
  */
+
+/*****ABOVE IN LINES 164, 177*****/
 /**
  * 8. To practice posting data, we'll create a system to "favourite" certain images.
  * - The skeleton of this function has already been created for you.
@@ -133,7 +234,38 @@ async function handleBreedSelect() {
  * - You can call this function by clicking on the heart at the top right of any image.
  */
 export async function favourite(imgId) {
-  // your code here
+  // axios
+  //   .get("https://api.thecatapi.com/v1/favourites")
+  //   .then((res) =>
+  //     res.data.forEach((i) =>
+  //       axios.delete(`https://api.thecatapi.com/v1/favourites/${i.id}`),
+  //     ),
+  //   );
+  //get all favourites
+  axios.get("https://api.thecatapi.com/v1/favourites").then((res) => {
+
+    console.log("FAVS=: ", res.data);
+
+    let deleted;
+
+    //looping over items
+    res.data.forEach((item) => {
+      if (item.image_id === imgId) {
+        //delete if already favorited
+        deleted = true;
+        axios.delete(`https://api.thecatapi.com/v1/favourites/${item.id}`);
+      }
+    });
+
+    if (!deleted) {
+      //else, we will post to the favourites
+      axios.post("https://api.thecatapi.com/v1/favourites", {
+        image_id: imgId,
+        sub_id: 'abc'
+      })
+        .then((res) => console.log(res.data));
+    }
+  });
 }
 
 /**
@@ -145,7 +277,21 @@ export async function favourite(imgId) {
  *    If that isn't in its own function, maybe it should be so you don't have to
  *    repeat yourself in this section.
  */
+getFavouritesBtn.addEventListener('click', getFavourites)
+function getFavourites() {
+  axios.get('https://api.thecatapi.com/v1/favourites')
+    .then(res => {
+      console.log('All Faves: ', res.data);
+      Carousel.clear();
 
+      res.data.forEach(item => {
+        const element = Carousel.createCarouselItem(item.image.url, item.image.image_id, item.image.id)
+
+        Carousel.appendCarousel(element);
+      });
+
+    })
+}
 /**
  * 10. Test your site, thoroughly!
  * - What happens when you try to load the Malayan breed?
